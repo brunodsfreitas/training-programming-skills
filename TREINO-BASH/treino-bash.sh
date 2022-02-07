192b23f9fbaa49e8c8dc55cc5148dc301c17c2d8
Bruno Freitas - Portable <brunods.freitas@hotmail.com>
2022-02-07 13:53:26 -0300

    LACOS-DE-REPETICAO

--- TREINO-BASH/treino-bash.sh
+++ TREINO-BASH/treino-bash.sh
@@ -38,80 +38,65 @@ echo "Você gosta de ouvir $nome_musica!"
 ### EXEMPLO DE SELECAO OU TOMADA DE DECISAO - IF
 echo "Digite um número qualquer:"
 read numero;
-if [ $numero -gt 20 ];
-then
+if [ $numero -gt 20 ]; then
   echo "Este número é maior que 20!"
 fi
 
-if [ 1 -eq 1 ]; #igual
-then
+if [ 1 -eq 1 ]; then #igual
   echo "1 eq 1 = true"
 fi
 
-if [ 2 -ne 1 ]; #diferente
-then
+if [ 2 -ne 1 ]; then #diferente
   echo "2 ne 1 = true"
 fi
 
-if [ 2 -gt 1 ]; #maior
-then
+if [ 2 -gt 1 ]; then #maior
   echo "2 gt 1 = true"
 fi
 
-if [ 2 -lt 3 ]; #menor
-then
+if [ 2 -lt 3 ]; then #menor
   echo "2 lt 3 = true"
 fi
 
-if [ 2 -ge 2 ]; #maior ou igual
-then
+if [ 2 -ge 2 ]; then #maior ou igual
   echo "2 ge 2 = true"
 fi
 
-if [ 1 -le 1 ]; #menor ou igual
-then
+if [ 1 -le 1 ]; then #menor ou igual
   echo "1 le 1 = true"
 fi
 
-if [ "OI" = "OI" ]; #string igual
-then
+if [ "OI" = "OI" ]; then #string igual
   echo "OI = OI = true"
 fi
 
-if [ "OI" != "OLA" ]; #string diferente
-then
+if [ "OI" != "OLA" ]; then #string diferente
   echo "OI != OLA = true"
 fi
 
-if [ -n "OI" ]; #comprimento da string diferente de 0
-then
+if [ -n "OI" ]; then #comprimento da string diferente de 0
   echo "n OI = true"
 fi
 
-if [ -z "" ]; #comprimento da string igual 0
-then
+if [ -z "" ]; then #comprimento da string igual 0
   echo "z '' = true"
 fi
 
-if [ -e "/var/log/lastlog" ]; #verifica se arquivo existe
-then
+if [ -e "/var/log/lastlog" ]; then #verifica se arquivo existe
   echo "e /var/log/lastlog = true"
 fi
 
-if [ -d "/var/log" ]; #verifica se a pasta existe
-then
+if [ -d "/var/log" ]; then #verifica se a pasta existe
   echo "d /var/log = true"
 fi
 
-if [ -f "/var/log/lastlog" ]; #verifica se é arquivo regular (documento, foto, programa, planilha)
-then
+if [ -f "/var/log/lastlog" ]; then #verifica se é arquivo regular (documento, foto, programa, planilhas
   echo "f /var/log/lastlog = true"
 fi
 
 ### ELSE
 
-if [ "OI" != "OI" ]; #string diferente
-then
+if [ "OI" != "OI" ]; then #string diferente
     echo "OI != OI = true"
 else
     echo "OI != OI = false"
@@ -120,15 +105,12 @@ fi
 ### ELIF PARA MAIS CONDICOES A SEREM TESTADAS
 echo "digite 1:hora, 2 ou 3."
 read num_dig; 
-if [ $num_dig = 1 ];
-then
+if [ $num_dig = 1 ]; then
     data=$(date +"%T, %d/%m/%y, %A")
     echo "$data"
-elif [ $num_dig = 2 ];
-then
+elif [ $num_dig = 2 ]; then
     echo "two"
-elif [ $num_dig = 3 ];
-then
+elif [ $num_dig = 3 ]; then
     echo "three"
 fi
 
@@ -136,10 +118,41 @@ fi
 read num_dig;
 case $num_dig in 
     1)
-    echo "1"
-    ;;
+        echo "1" ;;
     2)
-    echo "2"
-    ;;
+        echo "2" ;;
 esac
 
+### LOOPS
+
+###FOR
+echo "Testando FOR:"
+for num in {15..0}; do ### REPETE 16 VEZES
+    echo "$num"
+done
+
+echo "Testando FOR SEQ:"
+for num in $(seq 0 5 100); do ### INICIA 0, CRESCE EM 5, ENCERRA EM 100
+    echo "$num"
+done
+
+echo "Testando FOR SEQ sem intervalo:"
+for num in $(seq 0 100); do ### INICIA 0, ENCERRA EM 100
+    echo "$num"
+done
+
+###WHILE
+echo "Testando While, digite um número, o 0 sai."
+read num_dig;
+while [ $num_dig != "0" ]; do
+    echo "O Dígito $num_dig está incorreto, tente novamente."
+    read num_dig;
+done
+
+echo "Contar até qual número positivo ?"
+read num_dig;
+i=1;
+while [ $i -le $num_dig ]; do
+    echo "$i"
+    ((i=$i+1))
+done
