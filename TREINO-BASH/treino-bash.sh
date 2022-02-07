27d7408c085f21ed465bf9e56661e0777d584520
Bruno Freitas - Portable <brunods.freitas@hotmail.com>
2022-02-07 10:38:26 -0300

    VARIAVEIS-E-COMANDOS

--- TREINO-BASH/treino-bash.sh
+++ TREINO-BASH/treino-bash.sh
@@ -2,3 +2,28 @@
 
 
 #Este é um comentário
+
+### VARIAVEL
+site=www.bruno.com
+echo "O site é: $site"
+
+### COMANDO EM VARIAVEL
+system_info=`df -h` # Também poderia ser system_info=$(df -h)
+echo "$system_info"
+
+### COMANDOS
+echo "Seu nome de usuário é:"
+whoami
+echo "Info de hora atual e tempo que o computador está ligado:"
+uptime
+echo "O script está executando do diretório:"
+pwd
+data=$(date +"%T, %d/%m/%y, %A")
+echo "Pegando a data do sistema: $data"
+
+### EXEMPLO CAPTURA DE INPUT
+echo "Qual o nome de uma de suas músicas favoritas?"
+read nome_musica;
+echo "Você gosta de ouvir $nome_musica!"
+
+### FIM v1
