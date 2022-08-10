3593f2a83f8e5d9946b20d3eeafd28058ae4bdf1
Bruno Freitas <brunods.freitas@hotmail.com>
2022-08-10 04:05:13 +0000

    min41.36

--- TREINO-REACT-JS/login-treino/src/App.js
+++ TREINO-REACT-JS/login-treino/src/App.js
@@ -1,7 +1,34 @@
-
+import jpIMG from './assets/jp.svg'
+import './styles.css'
 function App() {
   return (
-    <h1>Hello</h1>
+    <div className="container">
+      <div className="container-login">
+        <div className="wrap-login">
+          <div className="login-form">
+            <span className="login-form-tittle">Bem Vindo!</span>
+            <span className="login-form-tittle">
+              <img src={jpIMG} alt="Jovem Programador"/>
+              <div className='wrap-input'>
+                <input className="input" type="email"/>
+                <span className="focus-input" data-placeholder="Email"></span>
+              </div>
+              <div className="wrap-input">
+                <input className="input" type="password"/>
+                <span className="focus-input" data-placeholder="Password"></span>
+              </div>
+              <div className="container-login-form-btn">
+                <button className="container-login-form-btn">Login</button>
+              </div>
+              <div className="text-center">
+                <span className="txt1">Não possui conta?</span>
+                <a className="txt2" href="#">Criar conta.</a>
+              </div>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
